e30b213f976333afa6396e389704528dda3cdfa0
ykmojo <ykim@motherjones.com>
2018-04-18 22:33:44 +0000

    setting up template basics for recharge newsletter

--- recharge-template.php
+++ recharge-template.php
@@ -6,7 +6,12 @@
  */
 
 //Post and Request section
-
+if(isset($_REQUEST['lift_note']) && $_REQUEST['lift_note'] != "") {
+	$lift_note = trim($_REQUEST['lift_note']);
+}
+else {
+	$lift_note = "";
+}
 //newsletter sections
 
 //ads
@@ -14,4 +19,25 @@
 //HTML template
 
 //db operations
+
+$subject_line = addslashes(str_replace("â€”","–", $subject_line));
+$sub_code = addslashes($sub_code);
+$lift_note = addslashes($lift_note);
+$pixel_tracker = addslashes($pixel_tracker);
+$pixel_tracker2 = addslashes($pixel_tracker2);
+$billboard_url = addslashes($billboard_url);
+$billboard_image = addslashes($billboard_image);
+$advertiser_name = addslashes($advertiser_name);
+$billboard_url2 = addslashes($billboard_url2);
+$billboard_image2 = addslashes($billboard_image2);
+$advertiser_name2 = addslashes($advertiser_name2);
+$advertiser_name = addslashes($advertiser_name);
+
+
+
+
+//flush query and close db connections
+mysqli_query($db_connect, $run_qry) or die("Query did not run correctly". mysqli_error($db_connect));
+mysqli_close($db_connect);
+/* end db section */
 ?>
